63f6eab8b729f4bf3c6bcacd77ca32d655ed1cd0
njandersen <njandersen22@gmail.com>
2022-08-18 09:15:36 -0400

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Layout from "../components/layout";
+
+export default function aboutPantry() {
+  return (
+    <>
+      <h1>Pantry is a way to keep track of your groceries and recipes!</h1>
+      <h2>
+        <Link href="/">Back Home</Link>
+      </h2>
+    </>
+  );
+}
---
+++ posts/test-post.md
@@ -0,0 +1,6 @@
+---
+title: "Test Post"
+date: "2022-08-18"
+---
+
+This is a test post to test the site and test markdown files since I have never used markdown files before. I am interested to see how this works.
